1064d59651eadd8ceed1951fe6e041982978d985
Alexey Zakharov <alexey.zakharov@securrency.com>
2022-10-01 05:25:18 +0300

    Arrays duplicate zeros

---
+++ Arrays.Tests/DuplicateZerosTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class DuplicateZerosTests
+{
+    [Theory]
+    [InlineData(new int[] {1,0,2,3,0,4,5,0}, new int[] {1,0,0,2,3,0,0,4})]
+    [InlineData(new int[] {1,2,3}, new int[] {1,2,3})]
+    [InlineData(new int[] {0,0,1}, new int[] {0,0,0})]
+    public void DuplicateZeros(int[] actual, int[] expected) 
+    {
+        var sut = new DuplicateZeros();
+        actual = sut.Run(actual);
+        
+        Assert.Equal(expected, actual);
+    } 
+}
---
+++ Arrays/DuplicateZeros.cs
@@ -0,0 +1,28 @@
+﻿namespace Arrays;
+
+public class DuplicateZeros
+{
+    public int[] Run(int[] arr)
+    {
+        int[] newArray = new int[arr.Length];
+        int insertedPosition = 0;
+
+        for (int i = 0; insertedPosition < arr.Length; i++)
+        {
+            if (arr[i] == 0)
+            {
+                newArray[insertedPosition] = 0;
+                insertedPosition += 2;
+            }
+            else
+            {
+                newArray[insertedPosition] = arr[i];
+                insertedPosition++;
+            }
+        }
+
+        Array.Copy(newArray, arr, arr.Length);
+
+        return arr;
+    }
+}
