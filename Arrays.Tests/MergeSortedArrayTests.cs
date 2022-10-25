cd008ef22e3449b05bc5b21e79554deb27a63c37
Alexey Zakharov <alexey.zakharov@securrency.com>
2022-10-25 06:33:34 +0300

    MergeSortedArray

---
+++ Arrays.Tests/MergeSortedArrayTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class MergeSortedArrayTests
+{
+    [Theory]
+    [InlineData(new[] {1,2,3,0,0,0}, 3, new[]{2,5,6}, 3, new[] {1,2,2,3,5,6})]
+    [InlineData(new[] {1}, 1, new int[]{}, 0, new[] {1})]
+    [InlineData(new int[] {}, 0, new []{1}, 1, new[] {1})]
+    [InlineData(new int[] {0}, 0, new []{1}, 1, new[] {1})]
+    public void MergeSortedArray(int[] nums1, int m, int[] nums2, int n, int[] expected) 
+    {
+        var sut = new MergeSortedArray();
+        var actual = sut.Merge(nums1, m, nums2, n);
+        
+        Assert.Equal(expected, actual);
+    } 
+}
---
+++ Arrays/MergeSortedArray.cs
@@ -0,0 +1,27 @@
+﻿namespace Arrays;
+
+public class MergeSortedArray
+{
+    // M - representing the number of elements in nums1
+    // N - representing the number of elements in nums2
+    // Nums1 has a length m + n
+    // 
+    public int[] Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        if (m == 0 || nums1.Length == 0)
+        {
+            Array.Copy(nums2, nums1, n);
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                nums1[m] = nums2[i];
+                m++;
+            }
+            Array.Sort(nums1);
+        }
+        
+        return nums1;
+    }
+}
