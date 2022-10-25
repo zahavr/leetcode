749ff0d36029b2f137a1d1deb121532a502f82de
Alexey Zakharov <alexey.zakharov@securrency.com>
2022-10-25 08:11:16 +0300

    remove element

---
+++ Arrays.Tests/RemoveElementTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class RemoveElementTests
+{
+    [Theory]
+    [InlineData(new int[] { 3,2,2,3 }, 3 , 2)]
+    [InlineData(new int[] { 0,1,2,2,3,0,4,2 }, 2 , 5)]
+    public void RemoveElement(int[] arr, int value, int expected)
+    {
+        RemoveElement sut = new RemoveElement();
+
+        int actual = sut.Do(arr, value);
+        
+        Assert.Equal(expected, actual);
+    }
+    
+}
---
+++ Arrays/RemoveElement.cs
@@ -0,0 +1,26 @@
+﻿namespace Arrays;
+
+public class RemoveElement
+{
+    public int Do(int[] nums, int val)
+    {
+        int i = 0;
+        int j = 0;
+
+        while (i <= nums.Length - 1 && j <= nums.Length - 1)
+        {
+            if (nums[i] != val)
+            {
+                nums[j] = nums[i];
+                j++;
+                i++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        
+        return j;
+    }
+}
