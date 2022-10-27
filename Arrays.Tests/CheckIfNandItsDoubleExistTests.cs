21fb583c3c533736ea40fbb1d2d7735acfb06e70
Alexey Zakharov <alexey.zakharov@securrency.com>
2022-10-27 04:10:46 +0300

    CheckIfNandItsDoubleExist

---
+++ Arrays.Tests/CheckIfNandItsDoubleExistTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class CheckIfNandItsDoubleExistTests
+{
+    [Theory]
+    [InlineData(true, new int[] { 10, 2, 5, 3 })]
+    [InlineData(false, new int[] { 3, 1, 7, 11 })]
+    [InlineData(false, new int[] { -2,0,10,-19,4,6,-8 })]
+    public void CheckIfNandItsDoubleExist(bool expected, int[] arr)
+    {
+        var sut = new CheckIfNandItsDoubleExist();
+        var actual = sut.Do(arr);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Arrays/CheckIfNandItsDoubleExist.cs
@@ -0,0 +1,23 @@
+﻿namespace Arrays;
+
+public class CheckIfNandItsDoubleExist
+{
+    public bool Do(int[] arr)
+    {
+        if (arr.Length == 0)
+        {
+            return false;
+        }
+        
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr.Length; j++)
+            {
+                if (arr[i] == 2 * arr[j] && i!= j)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
