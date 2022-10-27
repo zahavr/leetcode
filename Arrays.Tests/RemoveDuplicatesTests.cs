e25a6efdb36ab088836c72fce1e029bb1185cf23
Alexey Zakharov <alexey.zakharov@securrency.com>
2022-10-27 03:48:56 +0300

    RemoveDuplicates

---
+++ Arrays.Tests/RemoveDuplicatesTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class RemoveDuplicatesTests
+{
+    [Theory]
+    [InlineData(5, new int[] {0,0,1,1,1,2,2,3,3,4})]
+    [InlineData(2, new int[] {1,1,2})]
+    public void RemoveDuplicates(int expected, int[] arr)
+    {
+        RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+        int actual = removeDuplicates.Do(arr);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Arrays/RemoveDuplicates.cs
@@ -0,0 +1,22 @@
+﻿namespace Arrays;
+
+public class RemoveDuplicates
+{
+    public int Do(int[] nums)
+    {
+        int temp = nums[0];
+        int length = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (temp < nums[i])
+            {
+                temp = nums[i];
+                nums[length] = temp;
+                length++;
+            }
+        }
+        
+        return length;
+    }
+}
