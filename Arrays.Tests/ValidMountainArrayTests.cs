48570e489b5a5c441771d4a9f3f1dab874feeeab
Alexey Zakharov <alexey.zakharov@securrency.com>
2022-10-27 06:08:59 +0300

    ValidMountainArray

---
+++ Arrays.Tests/ValidMountainArrayTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class ValidMountainArrayTests
+{
+    [Theory]
+    [InlineData(false, new int[] { 2, 1 })]
+    [InlineData(false, new int[] { 3, 5, 5 })]
+    [InlineData(true, new int[] { 0, 3, 2, 1 })]
+    public void ValidMountainArray(bool expected, int[] arr)
+    {
+        var sut = new ValidMountainArray();
+        var actual = sut.Do(arr);
+        
+        Assert.Equal(expected, actual);
+    }
+    
+}
---
+++ Arrays/ValidMountainArray.cs
@@ -0,0 +1,20 @@
+﻿namespace Arrays;
+
+public class ValidMountainArray
+{
+    public bool Do(int[] arr)
+    {
+        if (arr.Length < 3)
+            return false;
+        
+        int fromStart = 0, fromEnd = arr.Length - 1;
+
+        while (fromStart < arr.Length - 1 && arr[fromStart] < arr[fromStart + 1])
+            fromStart++;
+
+        while (fromEnd > 0 && arr[fromEnd] < arr[fromEnd - 1])
+            fromEnd--;
+        
+        return fromStart == fromEnd && fromStart > 0 && fromEnd < arr.Length;
+    }
+}
