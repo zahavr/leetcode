3486ca644c380e9dac901c9a43aea4892c059e11
Alexey Zakharov <alexey.zakharov@securrency.com>
2022-11-15 04:06:43 +0200

    Solved

---
+++ Arrays.Tests/GreatestElementOnRightSideTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class GreatestElementOnRightSideTests
+{
+    [Theory]
+    [InlineData(new int[] { 17,18,5,4,6,1}, new int[] { 18,6,6,6,1,-1 })]
+    [InlineData(new int[] { 400 }, new int[] { -1 })]
+    public void GreatestElementOnRightSide_Do_Success(int[] arr, int[] expected)
+    {
+        var sut = new GreatestElementOnRightSide();
+        var actual = sut.Do(arr);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Arrays/GreatestElementOnRightSide.cs
@@ -0,0 +1,48 @@
+﻿namespace Arrays;
+
+public class GreatestElementOnRightSide
+{
+    public int[] Do(int[] arr)
+    {
+        int max = arr[^1];
+        int i = arr.Length - 1;
+        int counter = arr.Length;
+
+        if (i == 0)
+        {
+            arr[0] = -1;
+            return arr;
+        }
+        
+        while (counter != 0)
+        {
+            if (arr[i] > max)
+            {
+                (max, arr[i]) = (arr[i], max);
+            }
+            else
+            {
+                arr[i] = max;
+            }
+            i--;
+            counter--;
+        }
+
+        arr[^1] = -1;
+        
+        return arr;
+    }
+    
+    //Best algo:
+    /* public int[] ReplaceElements(int[] arr) {
+	var max = -1;
+	for (var i = arr.Length - 1; i >= 0; i--) {
+		var tmp = arr[i];
+		arr[i] = max;
+		if (tmp > max) {
+			max = tmp;
+		}
+	}
+	return arr;
+}*/
+}
