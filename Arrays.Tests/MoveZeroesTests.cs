1bbc3674e1d3750450ad1a5ff9f1d1bbe3a93de9
Alexey Zakharov <alexey.zakharov@securrency.com>
2022-11-17 03:12:12 +0200

    Solved

---
+++ Arrays.Tests/MoveZeroesTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class MoveZeroesTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 3, 12, 0, 0 }, new[] { 0, 1, 0, 3, 12 })]
+    [InlineData(new[] { 1 }, new[] { 1})]
+    [InlineData(new[] { 0 }, new[] { 0 })]
+    public void MoveZeroes_Do_Success(int[] expected, int[] arr)
+    {
+        var sut = new MoveZeroes();
+        var actual = sut.Do(arr);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Arrays/MoveZeroes.cs
@@ -0,0 +1,21 @@
+﻿namespace Arrays;
+
+public class MoveZeroes
+{
+    public int[] Do(int[] nums)
+    {
+        var newArr = new int[nums.Length];
+        var positionInNewArray = 0;
+
+        for (int readIndex = 0; readIndex < nums.Length; readIndex++)
+        {
+            if (nums[readIndex] != 0)
+            {
+                newArr[positionInNewArray] = nums[readIndex];
+                positionInNewArray++;
+            }
+        }
+        Array.Copy(newArr, nums, newArr.Length);
+        return newArr;
+    }
+}
