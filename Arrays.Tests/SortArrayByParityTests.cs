32299b4c8ea28a6c51dc65c2a6736e38e3b45c65
Alexey Zakharov <alexey.zakharov@securrency.com>
2022-11-18 02:38:23 +0200

    Solved

---
+++ Arrays.Tests/SortArrayByParityTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class SortArrayByParityTests
+{
+    [Theory]
+    [InlineData(new int[] { 3, 1, 2, 4 }, new int[] { 4,2,1,3 })]
+    [InlineData(new int[] { 1,0,3 }, new int[] { 0, 3, 1 })]
+    public void SortArrayByParity_Do_Success(int[] nums, int[] expected)
+    {
+        var sut = new SortArrayByParity();
+        var actual = sut.Do(nums);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Arrays/SortArrayByParity.cs
@@ -0,0 +1,24 @@
+﻿namespace Arrays;
+
+public class SortArrayByParity
+{
+    public int[] Do(int[] nums)
+    {
+        int indexFromStart = 0, indexFromEnd = nums.Length - 1;
+
+        while (indexFromEnd >= indexFromStart)
+        {
+            if (nums[indexFromStart] % 2 != 0)
+            {
+                (nums[indexFromEnd], nums[indexFromStart]) = (nums[indexFromStart], nums[indexFromEnd]);
+                indexFromEnd--;
+            }
+            else
+            {
+                indexFromStart++;
+            }
+        }
+        
+        return nums;
+    }
+}
