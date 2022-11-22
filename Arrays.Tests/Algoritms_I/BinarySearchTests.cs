8090497451460b1f31c52ea6cfcb539b37d0055a
Alexey Zakharov <alexeywork34@gmail.com>
2022-11-22 07:04:13 +0200

    Solved with linear complexity

---
+++ Arrays.Tests/Algoritms_I/BinarySearchTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class BinarySearchTests
+{
+    [Theory]
+    [InlineData(new[] {-1,0,3,5,9,12}, 9, 4)]
+    [InlineData(new[] {-1,0,3,5,9,12}, 2, -1)]
+    public void BinarySearch_Do_Success(int[] nums, int target, int expected)
+    {
+        var sut = new BinarySearch();
+        var actual = sut.Do(nums, target);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Arrays/Algoritms_I/BinarySearch.cs
@@ -0,0 +1,22 @@
+﻿namespace Arrays;
+
+public class BinarySearch
+{
+    public int Do(int[] nums, int target)
+    {
+        //Linear complexity O(n);
+        int targetIndex = 0;
+
+        foreach (var t in nums)
+        {
+            if (t == target)
+            {
+                break;
+            }
+
+            targetIndex++;
+        }
+        
+        return targetIndex == nums.Length ? -1 : targetIndex;
+    }
+}
