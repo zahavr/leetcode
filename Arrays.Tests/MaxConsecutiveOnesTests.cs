6a538b19f51f58da7c2c8ed9f5dfb7f44e313a65
Alexey Zakharov <alexeywork34@gmail.com>
2022-11-22 06:46:04 +0200

    Solved

---
+++ Arrays.Tests/MaxConsecutiveOnesTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class MaxConsecutiveOnesTests
+{
+    [Theory]
+    [InlineData(new[] {1,0,1,1,0}, 4)]
+    [InlineData(new[] {1,0,1,1,0,1}, 4)]
+    public void MaxConsecutiveOnes_BrutForce_Success(int[] nums, int expected)
+    {
+        var sut = new MaxConsecutiveOnes();
+        var actual = sut.BrutForce(nums);
+        
+        Assert.Equal(expected, actual);
+    }
+    
+    [Theory]
+    [InlineData(new[] {1,0,1,1,0}, 4)]
+    [InlineData(new[] {1,0,1,1,0,1}, 4)]
+    public void MaxConsecutiveOnes_SlidingWindow_Success(int[] nums, int expected)
+    {
+        var sut = new MaxConsecutiveOnes();
+        var actual = sut.SlidingWindow(nums);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Arrays/MaxConsecutiveOnes.cs
@@ -0,0 +1,62 @@
+﻿namespace Arrays;
+
+public class MaxConsecutiveOnes
+{
+    public int BrutForce(int[] nums)
+    {
+        var maxSequence = 0;
+
+        for (int left = 0; left < nums.Length; left++)
+        {
+            int zeroNums = 0;
+
+            for (int right = left; right < nums.Length; right++)
+            {
+                if (nums[right] == 0)
+                {
+                    zeroNums++;
+                }
+
+                if (zeroNums <= 1)
+                {
+                    maxSequence = Math.Max(maxSequence, right - left + 1);
+                }
+            }
+            
+        }
+        
+        return maxSequence;
+    }
+
+    public int SlidingWindow(int[] nums)
+    {
+        var maxSequence = 0;
+        var left = 0;
+        var right = 0;
+        var zeroNums = 0;
+
+        while (right < nums.Length)
+        {
+            if (nums[right] == 0)
+            {
+                zeroNums++;
+            }
+
+            while (zeroNums == 2)
+            {
+                if (nums[left] == 0)
+                {
+                    zeroNums--;
+                }
+
+                left++;
+            }
+
+            maxSequence = Math.Max(maxSequence, right - left + 1);
+            right++;
+        }
+
+        Array.BinarySearch(nums, left);
+        return maxSequence;
+    }
+}
