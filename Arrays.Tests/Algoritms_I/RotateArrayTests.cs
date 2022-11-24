ce4d34657c57829596eed8f07c9081ce56a59e6e
Alexey Zakharov <alexeywork34@gmail.com>
2022-11-24 04:00:22 +0200

    Solved

---
+++ Arrays.Tests/Algoritms_I/RotateArrayTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class RotateArrayTests
+{
+    [Theory]
+    [InlineData(new[] {1,2,3,4,5,6,7}, 3, new[] {5,6,7,1,2,3,4})]
+    [InlineData(new[] {-1,-100,3,99}, 2, new[] {3,99,-1,-100})]
+    public void RotateArray_OnPlaceWithSort(int[] nums, int k, int[] expected)
+    {
+        var sut = new RotateArray();
+        int[] actual = sut.OnPlace(nums, k);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Arrays/Algoritms_I/RotateArray.cs
@@ -0,0 +1,25 @@
+﻿namespace Arrays;
+
+public class RotateArray
+{
+    public int[] OnPlace(int[] nums, int k)
+    {
+        k %= nums.Length;
+        int count = 0;
+
+        for (int i = 0; count < nums.Length; i++)
+        {
+            int current = i;
+            int previousNumber = nums[i];
+            do
+            {
+                int nextNumber = (current + k) % nums.Length;
+                (nums[nextNumber], previousNumber) = (previousNumber, nums[nextNumber]);
+                current = nextNumber;
+                count++;
+            } while (i != current);
+        }
+        
+        return nums;
+    }
+}
