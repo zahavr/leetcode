8401bea21ae7efc54476a84a0336dbea671b15e1
Alexey Zakharov <alexeywork34@gmail.com>
2022-11-25 15:10:30 +0200

    Solved

---
+++ Arrays.Tests/Algoritms_I/TwoSumsTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+using Xunit;
+
+namespace Arrays.Tests;
+
+public class TwoSumsTests
+{
+    [Theory]
+    [InlineData(new[] { 2,7,11,15 }, 9, new[] {1, 2})]
+    [InlineData(new[] { 2,3,4 }, 6, new[] {1, 3})]
+    [InlineData(new[] { -1, 0 }, -1, new[] {1, 2})]
+    public void TwoSums_Do_Success(int[] nums, int target, int[] expected)
+    {
+        var sut = new TwoSums();
+        var actual = sut.Do(nums, target);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Arrays/Algoritms_I/TwoSums.cs
@@ -0,0 +1,31 @@
+﻿namespace Arrays;
+
+public class TwoSums
+{
+    public int[] Do(int[] numbers, int target) {
+        int[] result = {int.MinValue, int.MinValue};
+        int left = 0;
+        int right = numbers.Length - 1;
+
+        while(left < right)
+        {
+            if(numbers[left] + numbers[right] == target)
+            {
+                result[0] = left + 1;
+                result[1] = right + 1;
+                return result;
+            }
+
+            if (numbers[left] + numbers[right] < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        
+        return result;
+    }
+}
