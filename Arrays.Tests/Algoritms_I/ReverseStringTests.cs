0dfc879fe7c654fc9526ed38cacd570ff8e51903
Alexey Zakharov <alexeywork34@gmail.com>
2022-11-25 15:40:33 +0200

    Solved

---
+++ Arrays.Tests/Algoritms_I/ReverseStringTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Xunit;
+
+namespace Arrays.Tests;
+
+public class ReverseStringTests
+{
+    [Theory]
+    [InlineData(new[] {'h','e','l','l','o'}, new[] {'o','l','l','e','h'})]
+    [InlineData(new[] {'H','a','n','n','a','h'}, new[] {'h','a','n','n','a','H'})]
+    public void ReverseString_Do_Success(char[] s, char[] expected)
+    {
+        var sut = new ReverseString();
+        char[] actual = sut.Do(s);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Arrays/Algoritms_I/ReverseString.cs
@@ -0,0 +1,18 @@
+﻿namespace Arrays;
+
+public class ReverseString
+{
+    public char[] Do(char[] s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            (s[left], s[right]) = (s[right], s[left]);
+            left++;
+            right--;
+        }
+        return s;
+    }
+}
