c0635e696c888299bcb64c8383bba66e7b988042
Alexey Zakharov <alexeywork34@gmail.com>
2022-11-26 04:03:13 +0200

    Solved

---
+++ Arrays.Tests/Algoritms_I/ReverseWordsInAStringIIITests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Arrays.Tests;
+
+public class ReverseWordsInAStringIIITests
+{
+    [Theory]
+    [InlineData("Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc")]
+    [InlineData("God Ding", "doG gniD")]
+    public void ReverseWordsInAStringIIITests_Do_Success(string s, string expected)
+    {
+        var sut = new ReverseWordsInAStringIII();
+        var actual = sut.Do(s);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Arrays/Algoritms_I/ReverseWordsInAStringIII.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Arrays;
+
+public class ReverseWordsInAStringIII
+{
+    public string Do(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+        var arr = s.Split(' ');
+        
+
+        foreach (string s1 in arr)
+        {
+            for (int i = s1.Length - 1; i >= 0; i--)
+            {
+                sb.Append(s1[i]);
+            }
+
+            sb.Append(' ');
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
